ef2cd8a529504d7d2443e0381007fa493b52a5a1
Alexander Timonenkov <alex@talkatoo.com>
2022-05-21 11:43:58 +0300

    First

--- cache.go
+++ cache.go
@@ -1,23 +1,70 @@
 package cache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
+
+type cacheItem struct {
+	value          string
+	expirationTime int64
+}
+
+func (c cacheItem) isExpired() bool {
+	return c.expirationTime != 0 && c.expirationTime < time.Now().UnixNano()
+}
 
 type Cache struct {
+	sync.RWMutex
+	items map[string]cacheItem
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		items: map[string]cacheItem{},
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
+	c.RLock()
+	defer c.Unlock()
 
+	item, ok := c.items[key]
+	if !ok || item.isExpired() {
+		return "", false
+	}
+
+	return item.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.PutTill(key, value, time.Time{})
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	c.RLock()
+	defer c.Unlock()
+
+	result := make([]string, 0)
+	for key, item := range c.items {
+		if !item.isExpired() {
+			result = append(result, key)
+		}
+	}
+
+	if len(result) == 0 {
+		return nil
+	}
+
+	return result
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.Lock()
+	defer c.Unlock()
+
+	c.items[key] = cacheItem{
+		value:          value,
+		expirationTime: deadline.UnixNano(),
+	}
 }
